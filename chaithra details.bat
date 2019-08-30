2586b939413175ba1af3dd288f3c3503d3abecfb
ramavarjah <cshekar.madhu@gmail.com>
2019-08-30 12:59:49 +0530

    flask links

---
+++ chaithra details.bat
@@ -0,0 +1,37 @@
+cd E:\GMES\testrepo\GMES Test Framework
+java -cp "lib\*";"bin" org.testng.TestNG testng.xml
+
+pause
+
+exit
+
+9535743456
+
+Chaithra's ID:
+
+85010110@bat.com
+Welcome@12345
+
+
+
+Admin/9
+sp19
+
+https://igo.bat.net/Citrix/BATWeb/
+
+
+smg-netwoirk request-> 
+
+8755
+
+
+
+My OneID:
+85039618 
+$8Ktvbsu 
+
+skip validation issue is 
+
+https://www.bradcypert.com/writing-a-restful-api-in-flask-sqlalchemy/ 
+
+https://www.pmg.com/blog/restful-api-design-with-flask-and-sqlalchemy/
